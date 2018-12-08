accf2f2e497800081541ee3f7f3f8f6122dc9c5f
inderSjs <40349728+inderSjs@users.noreply.github.com>
2018-12-07 21:01:39 -0800

    part 2

--- BankAccount.java
+++ BankAccount.java
@@ -4,6 +4,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class BankAccount {
 	
 	private int balance;
+	private Lock accountLock = new ReentrantLock();
 	
 	public BankAccount()
 	{
@@ -14,12 +15,18 @@ public class BankAccount {
 	
 	public void withdraw( int amount)
 	{
-		
+		accountLock.lock();
+		try
+		{
 			System.out.println("withdrawing: " + amount);
 			int newBalance = this.balance - amount;
 			this.balance = newBalance;
 			System.out.println("new balance is: "+ newBalance);
-		
+		}
+		finally
+		{
+			accountLock.unlock();
+		}
 		
 		
 		
@@ -29,12 +36,18 @@ public class BankAccount {
 	
 	public void deposit( int amount)
 	{
-		
+		accountLock.lock();
+		try
+		{
 			System.out.println("Depositing: " + amount);
 			int newBalance = this.balance + amount;
 			this.balance = newBalance;
 			System.out.println("new balance: "+ newBalance);
-		
+		}
+		finally
+		{
+			accountLock.unlock();
+		}
 		
 	}
 	
